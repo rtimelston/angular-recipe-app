54f1940e79d59991e179559e4729d6ddada28aea
rtimelston <13246079+rtimelston@users.noreply.github.com>
2021-06-11 18:29:32 -0700

    Chapter 3 - Displaying a List of Recipes Using the ngFor Directive

--- src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -11,7 +11,11 @@ export class RecipeListComponent implements OnInit {
     new Recipe(
     'Curried Tomato Lentil Soup',
     "This curried tomato lentil soup is one of my go-to pantry recipes! The ingredients are easy to keep on hand",
-    "https://cdn.loveandlemons.com/wp-content/uploads/2020/03/pantry-recipes-2-580x580.jpg")
+    "https://cdn.loveandlemons.com/wp-content/uploads/2020/03/pantry-recipes-2-580x580.jpg"),
+    new Recipe(
+    'Spaghetti Squash with Chickpeas and Kale',
+    "Sun-dried tomatoes give this vegan spaghetti squash recipe a savory pop. Hearty from chickpeas & kale, it's a great fall or winter weeknight dinner",
+    "https://cdn.loveandlemons.com/wp-content/uploads/2014/10/IMG_2014_10_27_01314-2.jpg")
   ];
 
   constructor() { }
